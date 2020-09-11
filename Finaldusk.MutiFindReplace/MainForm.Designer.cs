f748146c4764d93aa62fa8a5a243620d63ca5ffa
wangxinlei <wxl@wangxinlei.cn>
2020-09-11 20:18:20 +0800

    添加项目文件。

---
+++ Finaldusk.MutiFindReplace/MainForm.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Finaldusk.MutiFindReplace
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxTarget = new System.Windows.Forms.TextBox();
+            this.label_Tips = new System.Windows.Forms.Label();
+            this.ColumnFind = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnReplace = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button_DoIt = new System.Windows.Forms.Button();
+            this.button_paste = new System.Windows.Forms.Button();
+            this.dataGridViewKeyValues = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewKeyValues)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxTarget
+            // 
+            this.textBoxTarget.Location = new System.Drawing.Point(296, 35);
+            this.textBoxTarget.Multiline = true;
+            this.textBoxTarget.Name = "textBoxTarget";
+            this.textBoxTarget.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxTarget.Size = new System.Drawing.Size(492, 403);
+            this.textBoxTarget.TabIndex = 1;
+            // 
+            // label_Tips
+            // 
+            this.label_Tips.AutoSize = true;
+            this.label_Tips.Location = new System.Drawing.Point(15, 6);
+            this.label_Tips.Name = "label_Tips";
+            this.label_Tips.Size = new System.Drawing.Size(270, 17);
+            this.label_Tips.TabIndex = 2;
+            this.label_Tips.Text = "Tips:You can copy from Excel and paste here";
+            // 
+            // ColumnFind
+            // 
+            this.ColumnFind.HeaderText = "Find";
+            this.ColumnFind.Name = "ColumnFind";
+            // 
+            // ColumnReplace
+            // 
+            this.ColumnReplace.HeaderText = "Replace";
+            this.ColumnReplace.Name = "ColumnReplace";
+            // 
+            // button_DoIt
+            // 
+            this.button_DoIt.Location = new System.Drawing.Point(712, 6);
+            this.button_DoIt.Name = "button_DoIt";
+            this.button_DoIt.Size = new System.Drawing.Size(75, 23);
+            this.button_DoIt.TabIndex = 3;
+            this.button_DoIt.Text = "Do It !";
+            this.button_DoIt.UseVisualStyleBackColor = true;
+            this.button_DoIt.Click += new System.EventHandler(this.button_DoIt_Click);
+            // 
+            // button_paste
+            // 
+            this.button_paste.Location = new System.Drawing.Point(584, 6);
+            this.button_paste.Name = "button_paste";
+            this.button_paste.Size = new System.Drawing.Size(122, 23);
+            this.button_paste.TabIndex = 5;
+            this.button_paste.Text = "Fill with clipboard";
+            this.button_paste.UseVisualStyleBackColor = true;
+            this.button_paste.Click += new System.EventHandler(this.button_paste_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            // 
+            // dataGridViewKeyValues
+            // 
+            this.dataGridViewKeyValues.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewKeyValues.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnFind,
+            this.ColumnReplace});
+            this.dataGridViewKeyValues.Location = new System.Drawing.Point(12, 35);
+            this.dataGridViewKeyValues.Name = "dataGridViewKeyValues";
+            this.dataGridViewKeyValues.Size = new System.Drawing.Size(278, 403);
+            this.dataGridViewKeyValues.TabIndex = 0;
+            this.dataGridViewKeyValues.Text = "dataGridView1";
+            this.dataGridViewKeyValues.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.dataGridViewKeyValues_PreviewKeyDown);
+            this.Controls.Add(this.button_paste);
+            this.Controls.Add(this.button_DoIt);
+            this.Controls.Add(this.label_Tips);
+            this.Controls.Add(this.textBoxTarget);
+            this.Controls.Add(this.dataGridViewKeyValues);
+            this.Name = "MainForm";
+            this.Text = "MutiFindREplace";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewKeyValues)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxTarget;
+        private System.Windows.Forms.Label label_Tips;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnFind;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnReplace;
+        private System.Windows.Forms.Button button_DoIt;
+        private System.Windows.Forms.Button button_paste;
+        private System.Windows.Forms.DataGridView dataGridViewKeyValues;
+    }
+}
+
---
+++ Finaldusk.MutiFindReplace/MainForm.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Finaldusk.MutiFindReplace
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridViewKeyValues_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.V) // 判断是否按下ctrl+v
+            {
+                Paste(dataGridViewKeyValues, "", 0, false);//粘贴代码
+            }
+        }
+
+        private void button_paste_Click(object sender, EventArgs e)
+        {
+            textBoxTarget.Text= Clipboard.GetText();
+        }
+
+        private void button_DoIt_Click(object sender, EventArgs e)
+        {
+            var targettext = textBoxTarget.Text;
+            var dic = new Dictionary<string, string>();
+
+            foreach (DataGridViewRow row in dataGridViewKeyValues.Rows)
+            {
+                var find = row.Cells[0].Value?.ToString();
+                var replace = row.Cells[1].Value?.ToString();
+                if (!string.IsNullOrWhiteSpace(find) && !string.IsNullOrWhiteSpace(replace))
+                {
+                    dic.Add(find, replace);
+                }
+            }
+                 
+            foreach (var item in dic)
+            {
+                targettext = targettext.Replace(item.Key, item.Value);
+            }
+            textBoxTarget.Text = targettext;
+        }
+
+        private int Paste(DataGridView dgv, string pasteText, int kind, bool b_cut)
+        {
+            try
+            {
+                if (kind == 0)
+                {
+                    pasteText = Clipboard.GetText();
+                }
+                if (string.IsNullOrEmpty(pasteText))
+                    return -1;
+                int rowNum = 0;
+                int columnNum = 0;
+                for (int i = 0; i < pasteText.Length; i++)
+                {
+                    if (pasteText.Substring(i, 1) == "\t")
+                    {
+                        columnNum++;
+                    }
+                    if (pasteText.Substring(i, 1) == "\n")
+                    {
+                        rowNum++;
+                    }
+                }
+                object[,] data;
+                if (pasteText.Substring(pasteText.Length - 1, 1) == "\n")
+                {
+                    rowNum = rowNum - 1;
+                }
+                columnNum = columnNum / (rowNum + 1);
+                data = new object[rowNum + 1, columnNum + 1];
+
+                string rowStr;
+                for (int i = 0; i < (rowNum + 1); i++)
+                {
+                    for (int colIndex = 0; colIndex < (columnNum + 1); colIndex++)
+                    {
+                        rowStr = null;
+                        if (colIndex == columnNum && pasteText.IndexOf("\r") != -1)
+                        {
+                            rowStr = pasteText.Substring(0, pasteText.IndexOf("\r"));
+                        }
+                        if (colIndex == columnNum && pasteText.IndexOf("\r") == -1)
+                        {
+                            rowStr = pasteText.Substring(0);
+                        }
+                        if (colIndex != columnNum)
+                        {
+                            rowStr = pasteText.Substring(0, pasteText.IndexOf("\t"));
+                            pasteText = pasteText.Substring(pasteText.IndexOf("\t") + 1);
+                        }
+                        if (rowStr == string.Empty)
+                            rowStr = null;
+                        data[i, colIndex] = rowStr;
+                    }
+                    pasteText = pasteText.Substring(pasteText.IndexOf("\n") + 1);
+                }
+                int columnindex = -1, rowindex = -1;
+                int columnindextmp = -1, rowindextmp = -1;
+                if (dgv.SelectedCells.Count != 0)
+                {
+                    columnindextmp = dgv.SelectedCells[0].ColumnIndex;
+                    rowindextmp = dgv.SelectedCells[0].RowIndex;
+                }
+                foreach (DataGridViewCell cell in dgv.SelectedCells)
+                {
+
+                    columnindex = cell.ColumnIndex;
+                    if (columnindex > columnindextmp)
+                    {
+                        columnindex = columnindextmp;
+                    }
+                    else
+                        columnindextmp = columnindex;
+                    rowindex = cell.RowIndex;
+                    if (rowindex > rowindextmp)
+                    {
+                        rowindex = rowindextmp;
+                        rowindextmp = rowindex;
+                    }
+                    else
+                        rowindextmp = rowindex;
+                }
+                if (kind == -1)
+                {
+                    columnindex = 0;
+                    rowindex = 0;
+                }
+
+                if (rowindex + rowNum + 1 > dgv.RowCount)
+                {
+                    int mm = rowNum + rowindex + 1 - dgv.RowCount;
+                    for (int ii = 0; ii < mm + 1; ii++)
+                    {
+                        dgv.DataBindings.Clear();
+
+                        dgv.Rows.Add();
+                    }
+                }
+
+                if (columnindex + columnNum + 1 > dgv.ColumnCount)
+                {
+                    int mmm = columnNum + columnindex + 1 - dgv.ColumnCount;
+                    for (int iii = 0; iii < mmm; iii++)
+                    {
+                        dgv.DataBindings.Clear();
+                        DataGridViewTextBoxColumn colum = new DataGridViewTextBoxColumn();
+                        dgv.Columns.Insert(columnindex + 1, colum);
+                    }
+                }
+
+                for (int j = 0; j < (rowNum + 1); j++)
+                {
+                    for (int colIndex = 0; colIndex < (columnNum + 1); colIndex++)
+                    {
+                        if (colIndex + columnindex < dgv.Columns.Count)
+                        {
+                            if (dgv.Columns[colIndex + columnindex].CellType.Name == "DataGridViewTextBoxCell")
+                            {
+                                if (dgv.Rows[j + rowindex].Cells[colIndex + columnindex].ReadOnly == false)
+                                {
+                                    dgv.Rows[j + rowindex].Cells[colIndex + columnindex].Value = data[j, colIndex];
+                                    dgv.Rows[j + rowindex].Cells[colIndex + columnindex].Selected = true;
+                                }
+                            }
+                        }
+                    }
+                }
+                if (b_cut)
+                    Clipboard.Clear();
+                return 1;
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+    }
+}
